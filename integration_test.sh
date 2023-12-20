d105542885ae03f1489121a75e2ded54cfc37d2b
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-12-20 15:06:51 +0200

    Initial intergration test for MASQ implemented.

---
+++ integration_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+./prepare_example_references.sh
+
+rm -rf example_outputs
+rm -rf test_example1
+rm -rf test_example2
+
+tar zxvf example_outputs.tar.gz
+
+snakemake -j 10
+
+diff -r -x logs -x fastqc \
+    example_outputs/test_example1 \
+    test_example1 || { echo 'test_example1 differs from the expected' >&2; exit 1; }
+diff -r  -x logs -x fastqc \
+    example_outputs/test_example2 \
+    test_example2 || { echo 'test_example2 differs from the expected' >&2; exit 1; }
+
--- prepare_example_references.sh
+++ prepare_example_references.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-mkdir references; cd references
-mkdir hg19; cd hg19
-wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
-gunzip hg19.fa.gz
-python ../../scripts/make_ref_genome_pickle.py hg19.fa hg19.seq_dic.cpickle
+
+mkdir -p references; cd references
+mkdir -p hg19; cd hg19
+
+if [[ ! -f hg19.fa ]]; then
+    wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
+    gunzip hg19.fa.gz
+fi
+
+if [[ ! -f hg19.seq_dic.cpickle ]]; then
+    python ../../scripts/make_ref_genome_pickle.py hg19.fa hg19.seq_dic.cpickle
+fi
+
 cd ../..
