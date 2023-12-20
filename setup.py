1782a74acc425fb11399252929affafb7cdd2a3a
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-12-20 15:22:51 +0200

    masq python package started.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+setuptools.setup(
+    name="masq",
+    url="https://github.com/amoffitt/MASQ",
+    packages=setuptools.find_packages(
+        where=".", exclude=[
+            "tests.*", "tests"
+        ],
+    ),
+    package_data={
+        "masq": ["py.typed"],
+    },
+    scripts=[
+    ],
+    entry_points="""
+    [console_scripts]
+
+    """,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
