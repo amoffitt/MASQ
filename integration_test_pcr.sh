c03a21c436569010b51e3b69108b05fff6eec706
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-03-21 10:30:27 +0200

    WIP: Setup new standardPCR example started.

---
+++ integration_test_pcr.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+pip install -e .
+
+./prepare_example_references.sh
+
+rm -rf example_outputs
+rm -rf test_example1
+rm -rf test_example2
+rm -rf combined
+rm -rf .snakemake
+rm -rf config.yaml
+
+mkdir -p example_outputs
+tar zxf pcr_example_outputs.tar.gz -C example_outputs
+
+ln -s config.standardPCR.yaml config.yaml
+
+snakemake -j 5
+
+# diff -r -x logs -x fastqc \
+#     example_outputs/test_example1 \
+#     test_example1 || { echo 'test_example1 differs from the expected' >&2; exit 1; }
+# diff -r  -x logs -x fastqc \
+#     example_outputs/test_example2 \
+#     test_example2 || { echo 'test_example2 differs from the expected' >&2; exit 1; }
+
