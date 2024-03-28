0f8f56f352f71cef4fdb8fc9c61cd4a301c83e0a
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-03-28 16:54:02 +0200

    Extract function to find valid pairs for primers.

--- masq/primer_design/blat_helpers.py
+++ masq/primer_design/blat_helpers.py
@@ -2,6 +2,10 @@ import subprocess
 from typing import Any
 from collections import Counter
 
+from masq.primer_design.primer3_helpers import get_primer_coordinates
+from masq.primer_design.snp import snps_or_indels_in_region
+from masq.utils.reference_genome import ReferenceGenome
+
 
 def run_blat(
     inputfile: str,
@@ -65,3 +69,161 @@ def process_blat_results(
                     blat_hits[snpid][leftright] = Counter()
                     blat_hits[snpid][leftright].update([primerid])
     return blat_hits
+
+
+def find_valid_pairs_of_primers_based_on_blat_hits(
+    snpdict: dict[str, dict[str, Any]],
+    primer3results: dict[str, Any],
+    blat_hits: dict[str, Any],
+    ref_genome: ReferenceGenome,
+    config: dict[str, Any],
+) -> dict[str, Any]:
+    """Look for valid pairs of primers based on blat hits.
+
+    Also check for SNPs in primer pairs
+    """
+    valid_primer_pairs = {}
+    best_primer_pair = {}
+    bam = config["wgs_bam"]
+
+    for snpid, counts in blat_hits.items():
+        perfectfound = False
+        perfectleft = []
+        perfectright = []
+        okleft = []
+        okright = []
+
+        print(snpid)
+        print(counts)
+        print("")
+
+        if 'LEFT' in counts:
+            for pid, ct in counts['LEFT'].items():
+                if ct == config['blat_perfect_num_hits']:
+                    perfectleft.append(pid)
+                    print(f"Perfect left: {snpid} - {pid}")
+                elif ct < config['blat_ok_num_hits']:
+                    okleft.append(pid)
+                    print(f"OK left: {snpid} - {pid}")
+        if 'RIGHT' in counts:
+            for pid, ct in counts['RIGHT'].items():
+                if ct == config['blat_perfect_num_hits']:
+                    perfectright.append(pid)
+                    print(f"Perfect right: {snpid} - {pid}")
+                elif ct < config['blat_ok_num_hits']:
+                    okright.append(pid)
+                    print(f"OK right: {snpid} - {pid}")
+
+        # check for perfect pair
+        perfectpairs = list(set(perfectleft).intersection(perfectright))
+        if len(perfectpairs) > 0:
+            valid_primer_pairs[snpid] = perfectpairs
+            ps = valid_primer_pairs[snpid]
+            # check perfect pairs for snps in primers before skipping next step
+            for p in ps:
+                (primerstringL, primerstringR) = get_primer_coordinates(
+                    p, snpid, primer3results, snpdict)
+                [snp_positionsL, seq_positions, snp_alt_bases, region_ref_seq] = \
+                    snps_or_indels_in_region(
+                        bam, primerstringL,
+                        ref_genome,
+                        basequal_cutoff=config['basequal_cutoff'],
+                        vaf_cutoff=config['vaf_cutoff'],
+                        indelaf_cutoff=config['indelaf_cutoff'],
+                        var_count_cutoff=config['var_count_cutoff'],
+                        indel_count_cutoff=config['indel_count_cutoff'])
+                [snp_positionsR, seq_positions, snp_alt_bases, region_ref_seq] = \
+                    snps_or_indels_in_region(
+                        bam, primerstringR,
+                        ref_genome,
+                        basequal_cutoff=config['basequal_cutoff'],
+                        vaf_cutoff=config['vaf_cutoff'],
+                        indelaf_cutoff=config['indelaf_cutoff'],
+                        var_count_cutoff=config['var_count_cutoff'],
+                        indel_count_cutoff=config['indel_count_cutoff'])
+                if (len(snp_positionsL) > 0) or (len(snp_positionsR) > 0):
+                    valid_primer_pairs[snpid].remove(p)
+                    print(f"Found SNP in primer pair: {p}")
+                    print(f"Left primer: {primerstringL}")
+                    print(f"Right primer: {primerstringR}")
+            if len(valid_primer_pairs[snpid]) > 0:
+                perfectfound = True
+
+        if not perfectfound:
+            # check for one perfect and one ok
+            ok_perf_pairs = list(set(perfectleft).intersection(okright))
+            ok_perf_pairs.extend(list(set(perfectright).intersection(okleft)))
+
+            best_pairs = []
+            # min hits combined across 2 primers
+            m = config['blat_perfect_num_hits'] + config['blat_ok_num_hits']
+            for p in ok_perf_pairs:
+                m_obs = blat_hits[snpid]['LEFT'][p] + \
+                    blat_hits[snpid]['RIGHT'][p]
+                if m_obs < m:
+                    m = m_obs
+                    best_pairs = [p]
+                elif m_obs == m:
+                    best_pairs.append(p)
+
+            valid_primer_pairs[snpid] = best_pairs
+
+        print(valid_primer_pairs[snpid])
+        print("Has valid primer pairs before SNP checking")
+        # Further selection based on product size (larger is better)
+        if len(valid_primer_pairs[snpid]) > 0:
+            m = 0
+            ps = valid_primer_pairs[snpid]
+            # sort so ties are broken by lowest number, which has best score
+            # from primer3
+            ps.sort(key=float)
+            print(ps)
+
+            ps_no_snps = list(ps)
+            # Check valid primer pairs for snps in primer, drop if SNP in
+            # primer
+            for p in ps:
+                (primerstringL, primerstringR) = \
+                    get_primer_coordinates(p, snpid, primer3results, snpdict)
+                [snp_positionsL, seq_positions, snp_alt_bases, region_ref_seq] = \
+                    snps_or_indels_in_region(
+                        bam, primerstringL,
+                        ref_genome,
+                        basequal_cutoff=config['basequal_cutoff'],
+                        vaf_cutoff=config['vaf_cutoff'],
+                        indelaf_cutoff=config['indelaf_cutoff'],
+                        var_count_cutoff=config['var_count_cutoff'],
+                        indel_count_cutoff=config['indel_count_cutoff'])
+                [snp_positionsR, seq_positions, snp_alt_bases, region_ref_seq] = \
+                    snps_or_indels_in_region(
+                        bam, primerstringR,
+                        ref_genome,
+                        basequal_cutoff=config['basequal_cutoff'],
+                        vaf_cutoff=config['vaf_cutoff'],
+                        indelaf_cutoff=config['indelaf_cutoff'],
+                        var_count_cutoff=config['var_count_cutoff'],
+                        indel_count_cutoff=config['indel_count_cutoff'])
+                if (len(snp_positionsL) > 0) or (len(snp_positionsR) > 0):
+                    print(f"Found SNP in primer pair: {p}")
+                    print(f"Left primer: {primerstringL}")
+                    print(f"Right primer: {primerstringR}")
+                    ps_no_snps.remove(p)
+
+            # Still has valid primer options
+            if len(ps_no_snps) > 0:
+                m = 0
+                for p in ps_no_snps:
+                    prodsize = int(
+                        primer3results[snpid][f"PRIMER_PAIR_{p}_PRODUCT_SIZE"])
+                    print(f"{prodsize}: {p}")
+                    if prodsize > m:
+                        m = prodsize
+                        bestprimer = p
+                        print("bigger product")
+                best_primer_pair[snpid] = bestprimer
+                print(bestprimer)
+        else:
+            snpdict[snpid]['status'] = 'drop'
+            snpdict[snpid]['drop_reason'] = 'blat_hits'
+
+    return best_primer_pair
