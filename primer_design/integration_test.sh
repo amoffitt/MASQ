18328dfdba6d27fd8b96a348bd0e5b4245f40005
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-04-11 17:27:42 +0300

    Primer design script updated to update_snplist_with_enzyme_selection

--- primer_design/integration_test.sh
+++ primer_design/integration_test.sh
@@ -6,7 +6,7 @@ mkdir -p design_results
 python select_enzymes_for_snps.py \
     config.primerdesign.example.yaml 2>&1 > primerdesign.log
 
-diff snp_primer_design_results.primerdesign_example.txt \
+diff snp_primer_design_results.primerdesign_example_updated.txt \
     design_results/snp_primer_design_results.primerdesign_example.*.txt || \
     { echo 'ERROR: design_results/snp_primer_design_results.primerdesign_example.*.txt differs from the expected' >&2; exit 1; }
 
--- primer_design/integration_test_small.sh
+++ primer_design/integration_test_small.sh
@@ -6,7 +6,7 @@ mkdir -p design_results
 python select_enzymes_for_snps.py \
     config.primerdesign.example_small.yaml 2>&1 > primerdesign.log
 
-diff snp_primer_design_results.primerdesign_example_small.txt \
+diff snp_primer_design_results.primerdesign_example_small_updated.txt \
     design_results/snp_primer_design_results.primerdesign_example_small.*.txt || \
     { echo 'ERROR: design_results/snp_primer_design_results.primerdesign_example_small.*.txt differs from the expected' >&2; exit 1; }
 
