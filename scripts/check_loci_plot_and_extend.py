77cf39ccee3faf489302e403af2e5e3054c1056f
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-05-16 16:56:19 +0300

    Stop using pickled reference genome. Clean up.

--- scripts/check_loci_plot_and_extend.py
+++ scripts/check_loci_plot_and_extend.py
@@ -3,17 +3,9 @@ Check in WGS BAM files for the target regions to identify any nearby SNVs or Ind
 Plots the coverage and base breakdown of target regions from WGS BAMs
 '''
 
-import os,sys
-import numpy as np
-import gzip
-from collections import Counter, defaultdict
-import fileinput
-import operator
-import pickle
+import os
+from collections import defaultdict
 import time
-
-from masq.utils.io import load_snv_table, write_snv_table, tabprint
-from masq.utils.seqs import reverse_complement
 
 import matplotlib
 matplotlib.use('Agg')
@@ -27,6 +19,7 @@ from masq_helper_functions import convert_cigar_string
 from masq_helper_functions import setup_logger
 from masq.utils.reference_genome import ReferenceGenome
 from masq.utils.io import process_target_info
+from masq.utils.io import load_snv_table, write_snv_table, tabprint
 
 ########################################################################
 # Start timer
@@ -47,7 +40,7 @@ elif isinstance(snakemake.input.bam,str) and isinstance(snakemake.params.wgs_nam
     WGS_BAM = [snakemake.input.bam]
     WGS_NAME = [snakemake.params.wgs_name]
 else:
-    logger.error("WGS_BAM and WGS_BAM should be lists or strings")
+    log.error("WGS_BAM and WGS_BAM should be lists or strings")
 
 # # Input SNV table
 SNV_table = open(snakemake.input.SNV_table,'r')
@@ -74,12 +67,12 @@ BASE_COLORS = ["#00ABBA",
 ########################################################################
 ## load the genome sequence from a pickle file
 log.info('Loading reference genome pickle')
-seq_pickle = snakemake.params.wgs_ref
-seq_dic = pickle.load(open(seq_pickle, 'rb'))
-log.info('Done loading reference genome pickle')
+# seq_pickle = snakemake.params.wgs_ref
+# seq_dic = pickle.load(open(seq_pickle, 'rb'))
 
 ref_genome = ReferenceGenome(snakemake.params.wgs_ref_fa)
 ref_genome.open()
+log.info('Done loading reference genome pickle')
 
 ########################################################################
 # Load the input SNV table
@@ -93,67 +86,6 @@ log.info('Done loading SNV table')
 ########################################################################
 
 # Process input file to extend it with strand info and get coordinates of seq
-log.info('Parsing specific SNV info fields')
-
-# target_info = []
-# target_locs_array = [list(map(int, x.split(";") ) ) if len(x)>0 else [] for x in snv_info['target_locs']]
-
-# for i,loc in enumerate(snv_info['loc']):
-#     chrom=snv_info['chr'][i]
-#     pos = int(snv_info['posi'][i])
-#     target_locs = target_locs_array[i]
-#     aml_loc = target_locs[0]
-#     log.debug('Position: %d' % pos)
-#     log.debug('AML Loc: %d' % aml_loc)
-#     int_seq = snv_info['trimmed-target-seq'][i]
-#     length = len(int_seq)
-
-#     if chrom=="0":
-#         strand="+"
-#         start=1
-#         end=100
-#         targets=target_locs
-#     elif ('strand' in snv_info.keys()) and ('fragment-start' in snv_info.keys()) and ('fragment-end' in snv_info.keys()) and ('add-targets' in snv_info.keys()):
-#         strand=snv_info['strand'][i]
-#         start=snv_info['fragment-start'][i]
-#         end=snv_info['fragment-end'][i]
-#         end=snv_info['add-targets'][i]
-#     else:
-#         log.info('Inferring strand, start, and end from match to reference genome')
-#         # identifying start and end genomic positions of the targeted region
-#         # and getting target sequence from ref genome
-#         top_start = pos - 1 - aml_loc
-#         top_end = top_start + length
-#         # top_match = seq_dic[chrom][top_start:top_end]
-#         top_match = ref_genome.get_sequence(chrom, top_start, top_end)
-#         # if the target sequence was on the bottom strand...
-#         bottom_start = pos - (length - aml_loc)
-#         bottom_end = bottom_start + length
-#         # bottom_match = reverse_complement(seq_dic[chrom][bottom_start:bottom_end])
-#         bottom_match = reverse_complement(
-#             ref_genome.get_sequence(chrom, bottom_start, bottom_end)
-#         )
-
-#         log.debug('Target seq : %s' % int_seq)
-#         log.debug('Top match  : %s' % top_match)
-#         log.debug('Btm match  : %s' % bottom_match)
-
-#         # check which strand the sequence came from and set coordinates
-#         if top_match == int_seq:
-#             log.debug('Locus %s: positive strand' % (loc))
-#             strand = "+"
-#             start = top_start
-#             end = top_end
-#             targets = target_locs
-#         else:
-#             log.debug('Locus %s: negative strand' % (loc))
-#             strand = "-"
-#             start = bottom_start
-#             end = bottom_end
-#             targets = [length - x - 1 for x in target_locs]
-
-#     target_info.append([chrom, strand, start, end, targets])
-
 target_info = process_target_info(snv_info, ref_genome)
 
 ########################################################################
@@ -186,7 +118,7 @@ for chrom, strand, true_start, true_end, true_targets in target_info:
         loc_ind += 1
     else:
 
-        log.debug('Locus %d' % (loc_ind+1))
+        log.debug('Locus %d', (loc_ind+1))
         image_filename = list_of_plot_files[loc_ind]
         loc_ind += 1
 
@@ -195,11 +127,11 @@ for chrom, strand, true_start, true_end, true_targets in target_info:
         end = true_end + region_buffer
         targets = [x + region_buffer for x in true_targets]
         # get local sequence
-        local_seq = seq_dic[chrom][start:end]
-        log.debug('Chrom: %s' % chrom)
-        log.debug('start: %d' % start)
-        log.debug('end: %d' % end)
-        log.debug('Local_seq: %s' % local_seq)
+        local_seq = ref_genome.get_sequence(chrom, start, end)
+        log.debug('Chrom: %s', chrom)
+        log.debug('start: %d', start)
+        log.debug('end: %d', end)
+        log.debug('Local_seq: %s', local_seq)
         L = len(local_seq)
         # also in integer form
         local_int = np.array([BASE2INT[x] for x in local_seq])
@@ -208,11 +140,11 @@ for chrom, strand, true_start, true_end, true_targets in target_info:
 
         # for each sample
         fig = plt.figure(figsize=(20, 18))
-        locus_string = "%s:%d-%d %s" % (chrom, start, end, strand)
+        locus_string = f"{chrom}:{start}-{end} {strand}"
         fig.suptitle(locus_string, fontsize=20)
         ind = 1
         for sample_name,sample_bam_fn in zip(WGS_NAME,WGS_BAM):
-            log.info('Plotting locus %d' % loc_ind)
+            log.info('Plotting locus %d', loc_ind)
             # Set up the plot
             ax1 = fig.add_subplot(len(WGS_NAME), 1, ind)
             ax2 = ax1.twinx()
@@ -230,13 +162,12 @@ for chrom, strand, true_start, true_end, true_targets in target_info:
 
             # store as pairs
             read_names = []
-            read_pair_dict = dict()
+            read_pair_dict = defaultdict(list)
             for read in read_iterator:
-                try:
-                    read_pair_dict[read.qname].append(read)  # if pair is there
-                except:
-                    read_pair_dict[read.qname] = [read]  # if pair is absent
-                    read_names.append(read.qname)  # keys to dictionary
+                qname = read.query_name
+                if qname not in read_pair_dict:
+                    read_names.append(qname)
+                read_pair_dict[qname].append(read)  # if pair is there
 
             N = len(read_names)  # number of read pairs in this region
             # store an integer at every position for each read(pair) in interval
@@ -248,6 +179,8 @@ for chrom, strand, true_start, true_end, true_targets in target_info:
                 for read in reads:
                     read_sequence = read.query_sequence
                     read_qualities = read.query_qualities
+                    if read_sequence is None or read_qualities is None:
+                        continue
                     # get_aligned_pairs:
                     # returns paired list of ref seq position and read seq position
                     for rindex, pindex in read.get_aligned_pairs():
@@ -256,41 +189,42 @@ for chrom, strand, true_start, true_end, true_targets in target_info:
                         # pindex: position in reference genome
                         # if there's a base in the read
                         # and we are in range
-                        if rindex is not None and pindex is not None:
-                            # Separated these lines, added pindex None check
-                            if pindex >= start and pindex < end:
-                                # compute the base score
-                                base_score = read_qualities[rindex]
-                                # if it's good enough
-                                if base_score >= MIN_QUAL:
-                                    # check if there is already a value in place
-                                    current = read_stack[rcounter, pindex - start]
-                                    base = read_sequence[rindex]
-                                    if base == "N":
-                                        baseint = 0
-                                    else:
-                                        # A-1, C-2, G-3, T-4
-                                        baseint = BASE2INT[base] + 1
-                                    # if there is no value, store the value
-                                    if current == 0:
-                                        read_stack[rcounter, pindex - start] = baseint
-                                    else:
-                                        # if there is a mismatch between the two reads,
-                                        # set value back to 0
-                                        # this value is just for the 2 paired reads
-                                        if current != baseint:
-                                            read_stack[rcounter, pindex - start] = 0
-            summary = []
+                        if rindex is None or pindex is None:
+                            continue
+                        # Separated these lines, added pindex None check
+                        if start <= pindex < end:
+                            # compute the base score
+                            base_score = read_qualities[rindex]
+                            # if it's good enough
+                            if base_score >= MIN_QUAL:
+                                # check if there is already a value in place
+                                current = read_stack[rcounter, pindex - start]
+                                base = read_sequence[rindex]
+                                if base == "N":
+                                    baseint = 0
+                                else:
+                                    # A-1, C-2, G-3, T-4
+                                    baseint = BASE2INT[base] + 1
+                                # if there is no value, store the value
+                                if current == 0:
+                                    read_stack[rcounter, pindex - start] = baseint
+                                else:
+                                    # if there is a mismatch between the two reads,
+                                    # set value back to 0
+                                    # this value is just for the 2 paired reads
+                                    if current != baseint:
+                                        read_stack[rcounter, pindex - start] = 0
+            summary_list = []
             # iterating over numpy array - by rows
             # transpose first to iterate over positions
             for x in read_stack.transpose():
                 # gets counts of N,A,C,G,T per position as array
                 # append to summary list
-                summary.append(np.bincount(x, minlength=5))
+                summary_list.append(np.bincount(x, minlength=5))
             # convert summary to array, fills by row
             # drop the N count, and transpose again
             # .T tranposes, only if num dimensions>1
-            summary = np.array(summary)[:, 1:].T
+            summary = np.array(summary_list)[:, 1:].T
             # now we have base (4) by position array as summary
             # base_cover: coverage at each position (sum of A/C/G/T counts)
             base_cover = np.sum(summary, axis=0)
@@ -398,6 +332,6 @@ outfile.close()
 # End timer
 t1 = time.time()
 td = (t1 - t0) / 60
-log.info("Done in %0.2f minutes" % td)
+log.info("Done in %0.2f minutes", td)
 
 ########################################################################
